2522e0f421ee1070316284b0c30aa4026bdf9380
lethargilistic <mikeoverby@outlook.com>
2017-09-06 23:54:08 -0400

    Adding __all__

--- spydes/__init__.py
+++ spydes/__init__.py
@@ -1,1 +1,5 @@
+__all__ = ['Card', 'Deck', 'Hand', 'Suit', 'Value', 
+           'all_suits', 'red_suits', 'black_suits',
+           'all_values', 'face_values', 'broadway_values',
+           'number_values']
 from .spydes import *
