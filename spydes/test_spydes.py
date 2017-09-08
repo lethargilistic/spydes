c984cff58e8e4aa2514411e2cac2ab4d1179617a
lethargilistic <mikeoverby@outlook.com>
2017-09-07 20:24:47 -0400

    test deck unicards

--- spydes/test_spydes.py
+++ spydes/test_spydes.py
@@ -12,3 +12,14 @@ def test_Deck_rotate_once():
     d = Deck([Card(Suit.heart, Value.ace), Card(Suit.club, Value.ace), Card(Suit.spade, Value.ace)])
     d.rotate()
     assert d == [Card(Suit.club, Value.ace), Card(Suit.spade, Value.ace), Card(Suit.heart, Value.ace)]
+
+def test_Card_same_value():
+    first_card = Card(Suit.club, Value.three)
+    second_card = Card(Suit.joker, Value.three)
+    assert first_card.same_value(second_card)
+
+def test_Deck_unicards_varyjokers():
+    d = Deck()
+    d.new_pack(jokers=5)
+    correct = '🃟 🂿 🃏 🃟 🂿 🂡 🂢 🂣 🂤 🂥 🂦 🂧 🂨 🂩 🂪 🂫 🂭 🂮 🂱 🂲 🂳 🂴 🂵 🂶 🂷 🂸 🂹 🂺 🂻 🂽 🂾 🃁 🃂 🃃 🃄 🃅 🃆 🃇 🃈 🃉 🃊 🃋 🃍 🃎 🃑 🃒 🃓 🃔 🃕 🃖 🃗 🃘 🃙 🃚 🃛 🃝 🃞 '
+    assert d.unicard(vary_jokers=True) == correct
